54e27f511446305d5c799630dbd5d717aaf0fd7d
2dam <you@example.com>
2022-10-25 08:46:18 +0200

    completely functional code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.ResourceBundle;
+
+import client.Client;
+import server.Server;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    private static String rb = 
+        ResourceBundle
+            .getBundle("resources.connect")
+                .getString("TYPE")
+                    .toUpperCase();
+                    
+    public static void main(String[] args) {
+        switch (rb) {
+            case "CLIENT" : new Client().start();
+                break;
+            case "SERVER" : new Server().start();
+                break;
+            default : System.out.println("Invalid type.");
+                break; 
+        }
     }
 }
